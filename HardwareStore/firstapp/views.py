041accfc7a06ea3da90fe97625313f8baffc5e73
Your Name <you@example.com>
2025-01-27 09:41:00 +0300

    27.01.25

--- HardwareStore/firstapp/views.py
+++ HardwareStore/firstapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World! Это мой первый проект на Django!")
+    return render(request, 'index.html')
