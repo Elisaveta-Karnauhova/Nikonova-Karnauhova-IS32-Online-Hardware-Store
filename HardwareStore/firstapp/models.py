d845cca1bd454675afd30ab0238f426a114ae822
Your Name <you@example.com>
2025-02-05 12:49:22 +0300

    Разбираюсь со стилями

--- HardwareStore/firstapp/models.py
+++ HardwareStore/firstapp/models.py
@@ -3,4 +3,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=50)
     manufacturer = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    price = models.CharField(max_length=100)
+    available = models.CharField(max_length=50) 
+    amount = models.IntegerField(default=0)
     
